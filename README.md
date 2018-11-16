fd3a0db38a7217c9eec606f887fc6244c1661d43
Blake Tooth <blake.tooth@gmail.com>
2018-11-16 04:02:35 +0000

    First Draft of WozTax

---
+++ README.md
@@ -0,0 +1,8 @@
+# docassemble.Woztax
+
+A docassemble extension for collecting income and expense information and estimating Australian income tax.
+
+## Author
+
+Blake Tooth, blake.tooth@gmail.com
+
---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup, find_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.py', '*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.Woztax',
+      version='0.0.1',
+      description=('A docassemble extension for collecting income and expense information and estimating Australian income tax.'),
+      long_description=u'# docassemble.Woztax\n\nA docassemble extension for collecting income and expense information and estimating Australian income tax.\n\n## Author\n\nBlake Tooth, blake.tooth@gmail.com\n\n',
+      long_description_content_type='text/markdown',
+      author='Blake Tooth',
+      author_email='blake.tooth@gmail.com',
+      license='The MIT License (MIT)',
+      url='https://docassemble.org',
+      packages=find_packages(),
+      namespace_packages=['docassemble'],
+      install_requires=[],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/Woztax/', package='docassemble.Woztax'),
+     )
+
